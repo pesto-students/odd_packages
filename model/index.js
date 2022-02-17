f8de9678847c2e78947ea8d1279298af292366be
akashmalviya <akashmalviya1999@gmail.com>
2022-02-17 14:20:13 +0530

    Updates

--- model/index.js
+++ model/index.js
@@ -1,7 +1,9 @@
+
+
 export { default as User } from "./user";
 export { default as Driver } from "./driver";
 export { default as Admin } from "./admin";
 export { default as Order } from "./order";
 export { default as Vehicle } from "./vehicle";
-export { default as DriverStatics } from "/driver_Statics"
+export { default as DriverStatics } from "./driver_Statics"
 export { default as cors } from "cors"
